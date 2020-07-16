8efacba3c1453f5e032be6e02c992b1dfdd7d971
Mirko Reimuth <mirko.reimuth@gmail.com>
2020-07-16 15:47:00 +0200

    adding two more test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -2,7 +2,13 @@ package org.xpdojo.bank;
 
 public class Account {
     // change something to see if git push is possible...
+    private int balance;
+
     public int balance() {
-        return 0;
+        return this.balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -12,4 +12,19 @@ public class AccountTest {
         Account account=new Account();
         assertThat(account.balance()).isEqualTo(0);
     }
+
+    @Test
+    public void depositAnAmountToIncreaseBalance(){
+        Account account=new Account();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
+    @Test
+    public void depositMultiAmountsToIncreaseBalance(){
+        Account account=new Account();
+        account.deposit(10);
+        account.deposit(20);
+        assertThat(account.balance()).isEqualTo(30);
+    }
 }
